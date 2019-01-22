81433bb5bb353753b8023a24fbf00bf358eae10b
ThePraeceps <sunrob946@gmail.com>
2019-01-22 11:46:50 +0000

    Update install.sh
    Added Mass Storage

--- install.sh
+++ install.sh
@@ -2,6 +2,16 @@
 # Enable LibComposite
 modprobe libcomposite
 
+
+# Making USB Mass Storage File System
+dd if=/dev/zero of=~/usbdisk.img bs=1024 count=524288
+mkdosfs ~/usbdisk.img
+
+IMAGEFILE=~/usbdisk.img
+
+mkdir -p ${IMAGEFILE/img/d}
+mount -o loop,ro, -t vfat $IMAGEFILE ${IMAGEFILE/img/d}
+
 # Create gadget
 cd /sys/kernel/config/usb_gadget/
 mkdir ecoduck && cd ecoduck
@@ -32,6 +42,18 @@ echo 8 > functions/hid.usb0/report_length # 8-byte reports
 echo 1 > functions/hid.usb0/subclass
 xxd -r -ps /root/mydesc functions/hid.usb0/report_desc
 
+# Create Mass Storage function
+mkdir functions/mass_storage.usb0
+
+echo 1 > functions/mass_storage.usb0/stall
+echo 0 > functions/mass_storage.usb0/lun.0/cdrom
+echo 0 > functions/mass_storage.usb0/lun.0/ro
+echo 0 > functions/mass_storage.usb0/lun.0/nofua
+echo $IMAGEFILE > functions/mass_storage.usb0/lun.0/file
+
+echo $FILE > functions/mass_storage.usb0/lun.0/file
+
+
 # Create configuration
 mkdir configs/c.1
 mkdir configs/c.1/strings/0x409
@@ -42,6 +64,7 @@ echo "Test config" > configs/c.1/strings/0x409/configuration
 
 # Link HID function to configuration
 ln -s functions/hid.usb0 configs/c.1
+ln -s functions/mass_storage.usb0 configs/c.1/
 
 # Enable gadget
 ls /sys/class/udc > UDC
