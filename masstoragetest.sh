d9792738619e03b70d41ab0efd56823a7394d4b8
ThePraeceps <sunrob946@gmail.com>
2019-01-22 12:44:37 +0000

    Update masstoragetest.sh

--- masstoragetest.sh
+++ masstoragetest.sh
@@ -1,64 +1,54 @@
 #!/bin/bash
-# Enable LibComposite
-modprobe libcomposite
 
-echo "Making File System"
-# Making USB Mass Storage File System
-dd if=/dev/zero of=/ecoduck.img bs=1024 count=524288
-mkdosfs /ecoduck.img
-
-IMAGEFILE=/ecoduck.img
-
-mkdir /mnt/ecoduck
-mount -o loop,ro, -t vfat $IMAGEFILE /mnt/ecoduck
-
-echo "Creating gadget"
-# Create gadget
+# assumes a disk image exists here...
+FILE=/ecoduck.img
+mkdir -p /mnt/ecoduck
+mount -o loop,ro,offset=1048576 -t ext4 $FILE /mnt/ecoduck
 cd /sys/kernel/config/usb_gadget/
-mkdir ecomass && cd ecomass
 
+mkdir -p ecomass
+cd ecomass
+#echo '' > UDC
 
-# Add basic information
-
-echo 0x0104 > idProduct # Multifunction Composite Gadget
 echo 0x1d6b > idVendor # Linux Foundation
-echo 0x0100 > bcdDevice # Version 1.0.0
-echo 0x0200 > bcdUSB # USB 2.0
+echo 0x0104 > idProduct # Multifunction Composite Gadget
+echo 0x0100 > bcdDevice # v1.0.0
+echo 0x0200 > bcdUSB # USB2
 
-# echo 0x00 > bDeviceClass
-# echo 0x00 > bDeviceProtocol
-# echo 0x00 > bDeviceSubClass
-# echo 0x08 > bMaxPacketSize0
+mkdir -p strings/0x409
+echo "fedcba9876543210" > strings/0x409/serialnumber
+echo "Inverse Path" > strings/0x409/manufacturer 
+echo "USB Armory" > strings/0x409/product 
 
-# Create English locale
-mkdir strings/0x409
+N="usb0"
 
-echo "Team 404" > strings/0x409/manufacturer
-echo "Economical Duck" > strings/0x409/product
-echo "1337696969" > strings/0x409/serialnumber
-
-
-# Create Mass Storage function
-mkdir functions/mass_storage.usb0
-
-echo 1 > functions/mass_storage.usb0/stall
-echo 0 > functions/mass_storage.usb0/lun.0/cdrom
-echo 0 > functions/mass_storage.usb0/lun.0/ro
-echo 0 > functions/mass_storage.usb0/lun.0/nofua
-echo $IMAGEFILE > functions/mass_storage.usb0/lun.0/file
+mkdir -p functions/hid.$N
+mkdir -p functions/mass_storage.$N
 
 
-# Create configuration
-mkdir configs/c.1
-mkdir configs/c.1/strings/0x409
+echo 1 > functions/mass_storage.$N/stall
+echo 0 > functions/mass_storage.$N/lun.0/cdrom
+echo 0 > functions/mass_storage.$N/lun.0/ro
+echo 0 > functions/mass_storage.$N/lun.0/nofua
+echo $FILE > functions/mass_storage.$N/lun.0/file
 
-# echo 0x80 > configs/c.1/bmAttributes
-echo 200 > configs/c.1/MaxPower # 200 mA
-echo "Test config" > configs/c.1/strings/0x409/configuration
+echo 1 > functions/hid.0/protocol
+echo 1 > functions/hid.0/subclass
+echo 8 > functions/hid.0/report_length
+echo -ne \\x05\\x01\\x09\\x06\\xa1\\x01\\x05\\x07\\x19\\xe0\\x29\\xe7\\x15\\x00\\x25\\x01\\x75\\x01\\x95\\x08\\x81\\x02\\x95\\x01\\x75\\x08\\x81\\x03\\x95\\x05\\x75\\x01\\x05\\x08\\x19\\x01\\x29\\x05\\x91\\x02\\x95\\x01\\x75\\x03\\x91\\x03\\x95\\x06\\x75\\x08\\x15\\x00\\x25\\x65\\x05\\x07\\x19\\x00\\x29\\x65\\x81\\x00\\xc0 > functions/hid.0/report_desc
 
-echo "Starting gadget"
-# Link HID function to configuration
-ln -s functions/mass_storage.usb0 configs/c.1/
 
-# Enable gadget
+C=1
+mkdir -p configs/c.$C/strings/0x409
+
+echo 250 > configs/c.$C/MaxPower 
+
+ln -s functions/mass_storage.$N configs/c.$C/
+ln -s functions/hid.$N configs/c.$C/
+
+# it took a little while to find out that the i.MX53 uses
+# a ChipIdea core and thus "ci_hdrc.0" is the right driver 
+#echo ci_hdrc.0 > UDC
+
+# this lists available UDC drivers
 ls /sys/class/udc > UDC
