ab0e75d5d549d9d23a12f9aab8374cce442ac7de
ThePraeceps <sunrob946@gmail.com>
2019-01-23 17:51:48 +0000

    Fixed gadget remover

--- removegadget.sh
+++ removegadget.sh
@@ -4,13 +4,16 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
 fi
-"Removing gadget"
+echo "Removing gadget"
+
+N="usb0"
+C=1
 
 cd /sys/kernel/config/usb_gadget/ecoduck
 
 echo "" > UDC
 
-rm configs/c.$C/*
+rm configs/c.$C/*.$N
 rmdir configs/c.$C/strings/*
 rmdir configs/c.$C
 rmdir functions/*
