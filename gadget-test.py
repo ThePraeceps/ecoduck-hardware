17b9f5b8bdc640c91f61cbff54960d4aef26eedf
ThePraeceps <sunrob946@gmail.com>
2019-01-28 17:06:14 +0000

    Added network test

--- gadget-test.py
+++ gadget-test.py
@@ -1,8 +1,10 @@
 #!/bin/usr/env python3
-import os,signal
+import os,signal,io
 from time import sleep
 from subprocess import Popen, PIPE, check_output
+
 def write_report(report, path):
+	# Writes packet to given path
 	fd = os.open(path, os.O_RDWR)
 	os.write(fd, report)
 	os.close(fd)
@@ -68,8 +70,31 @@ def dummy_payload(path):
 	# Release al keys
 	write_report(b'\x00\x00\x00\x00\x00\x00\x00\x00',path)
 
+def network_test(path):
+	# ping 192.168.10.1 (The pi)
+	write_report(b'\x00\x00\x13\x00\x00\x00\x00\x00',path)
+	write_report(b'\x00\x00\x0C\x00\x00\x00\x00\x00',path)
+	write_report(b'\x00\x00\x11\x00\x00\x00\x00\x00',path)
+	write_report(b'\x00\x00\x0A\x00\x00\x00\x00\x00',path)
+	write_report(b'\x00\x00\x2c\x00\x00\x00\x00\x00',path)
+	write_report(b'\x00\x00\x1E\x00\x00\x00\x00\x00',path)
+	write_report(b'\x00\x00\x26\x00\x00\x00\x00\x00',path)
+	write_report(b'\x00\x00\x37\x00\x00\x00\x00\x00',path)
+	write_report(b'\x00\x00\x1E\x00\x00\x00\x00\x00',path)
+	write_report(b'\x00\x00\x23\x00\x00\x00\x00\x00',path)
+	write_report(b'\x00\x00\x1F\x00\x00\x00\x00\x00',path)
+	write_report(b'\x00\x00\x37\x00\x00\x00\x00\x00',path)
+	write_report(b'\x00\x00\x1E\x00\x00\x00\x00\x00',path)
+	write_report(b'\x00\x00\x27\x00\x00\x00\x00\x00',path)
+	write_report(b'\x00\x00\x37\x00\x00\x00\x00\x00',path)
+	write_report(b'\x00\x00\x1E\x00\x00\x00\x00\x00',path)
+	write_report(b'\x00\x00\x00\x00\x00\x00\x00\x00',path)
+	targetip=get_last_lease()
+	nmapresults=check_output("nmap -A " + target)
+	print(nmapresults)
 
 def wait_till_disconnect():
+	# Loops till electrical tests fails
 	print("Waiting for device removal")
 	while(electrical_test("/dev/hidg0",1)):
 		sleep(3)
@@ -77,9 +102,11 @@ def wait_till_disconnect():
 
 
 def timeout_handler():
+	# Helper function for electrical_test
 	raise Execption("Timeout")
 
 def electrical_test(path, timeout):
+	# Checks for a led HID packet from the host - proves target is connected, then resets capslock if it is on
 	signal.signal(signal.SIGALRM, timeout_handler)
 	signal.alarm(timeout)
 	try:
@@ -98,6 +125,13 @@ def electrical_test(path, timeout):
 		return False
 	signal.alarm(0)
 	return True
+
+def get_last_lease():
+	fd = io.open("/var/lib/misc/dnsmasq.leases", "r")
+	firstline = fd.readline()
+	columns=firstline.split(" ")
+	print("Found target IP: " + columns[2])
+	return columns[2]
 
 
 
