2ae490dbf2901ee757555207331aab195af550b6
ThePraeceps <sunrob946@gmail.com>
2019-01-28 17:19:37 +0000

    Added decode to nmap results

--- gadget-test.py
+++ gadget-test.py
@@ -92,7 +92,7 @@ def network_test(path):
 	write_report(b'\x00\x00\x00\x00\x00\x00\x00\x00',path)
 
 	targetip=get_last_lease()
-	nmapresults=check_output("/usr/bin/nmap -A " + targetip)
+	nmapresults=check_output("/usr/bin/nmap -A " + targetip).decode()
 	print(nmapresults)
 
 def wait_till_disconnect():
