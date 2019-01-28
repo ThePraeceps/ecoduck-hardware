5dcdd4ae936c53d705e774a4a25aef65014f0767
ThePraeceps <sunrob946@gmail.com>
2019-01-28 19:30:56 +0000

    Fixed gadget-remove for new gadgets

--- gadget-remove.sh
+++ gadget-remove.sh
@@ -9,9 +9,13 @@ echo "Removing gadget"
 N="usb0"
 C=1
 
-cd /sys/kernel/config/usb_gadget/ecoduck
+cd /sys/kernel/config/usb_gadget
 
-echo "" > UDC
+echo "" > ecoduck-simple/UDC
+echo "" > ecoduck-win/UDC
+echo "" > ecoduck-other/UDC
+
+cd ecoduck-simple
 
 rm configs/c.*/*.$N
 rmdir configs/c.*/strings/*
@@ -19,6 +23,28 @@ rmdir configs/c.*
 rmdir functions/*
 rmdir strings/*
 cd ..
-rmdir ecoduck
 
-echo "Gadget removed"
+cd ecoduck-win
+
+rm configs/c.*/*.$N
+rm os_desc/c.*
+rmdir configs/c.*/strings/*
+rmdir configs/c.*
+rmdir functions/*
+rmdir strings/*
+cd ..
+
+cd ecoduck-other
+
+rm configs/c.*/*.$N
+rmdir configs/c.*/strings/*
+rmdir configs/c.*
+rmdir functions/*
+rmdir strings/*
+cd ..
+
+rmdir ecoduck-simple
+rmdir ecoduck-win
+rmdir ecoduck-other
+
+echo "Gadgets removed"
